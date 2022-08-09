434ce8729ee00c171baf1435bbb807e0e34eeee7
GoOnNowGit <goonnowgittt@gmail.com>
2022-08-08 17:57:41 -1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name="dnscryptutils",
     version="0.0.1",
-    description="Create firewall rules from dnscrypt-proxy config",
+    description="Print out dnsstamp info from sources in your dnscrypt-config.toml configuration file",
     author="goonnowgit",
     author_email="goonnowgittt@gmail.com",
     packages=find_packages(where="src"),
